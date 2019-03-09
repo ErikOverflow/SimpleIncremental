a0b253ed6f8b12144c7bd2920748853a53e4b01d
Erik <>
2019-03-08 20:29:46 -0600

    Add Kenney packages and create main menu scene

---
+++ Simple Incremental/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadNextScene(Object scene)
+    {
+        SceneManager.LoadSceneAsync(scene.name);
+    }
+}
