80172d7a69a40de7d708b97c784366aeb4939cee
Erik <>
2019-03-10 21:25:22 -0500

    Created EnemyHook and EnemyTemplate that will automatically update enemy components when the template is changed

---
+++ Simple Incremental/Assets/Scripts/EnemyHook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHook : MonoBehaviour
+{
+    [SerializeField]
+    private EnemyTemplate enemyTemplate = null;
+
+    public void ChangeTemplate(EnemyTemplate enemyTemplate)
+    {
+        if (enemyTemplate != null)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            CharacterHealth characterHeatlh = GetComponent<CharacterHealth>();
+            CharacterLoot characterLoot = GetComponent<CharacterLoot>();
+            if(spriteRenderer != null)
+                spriteRenderer.sprite = enemyTemplate.sprite;
+            if(characterHeatlh != null)
+                characterHeatlh.maxHealth = enemyTemplate.health;
+            if(characterLoot != null)
+                characterLoot.coins = enemyTemplate.coins;
+        }
+    }
+
+    private void OnValidate()
+    {
+        ChangeTemplate(enemyTemplate);
+    }
+}
