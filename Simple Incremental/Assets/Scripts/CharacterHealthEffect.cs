8393e6eae3f3f01ad00c3ecd8eaf16eadc1e08c2
Raganvald <raganvald@gmail.com>
2019-03-12 00:52:37 -0700

    Adding particle system that plays when enemy is attacked.

---
+++ Simple Incremental/Assets/Scripts/CharacterHealthEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterHealth))]
+[RequireComponent(typeof(ParticleSystem))]
+public class CharacterHealthEffect : MonoBehaviour
+{
+    CharacterHealth characterHealth;
+    ParticleSystem system;
+
+    void Awake()
+    {
+        characterHealth = GetComponent<CharacterHealth>();
+        system = GetComponent<ParticleSystem>();
+    }
+    void Start()
+    {
+        characterHealth.HealthChanged += playHitEffect;
+    }
+
+    private void playHitEffect()
+    {
+        //When players health changes play damage partice effect
+        system.Play();
+    }
+}
