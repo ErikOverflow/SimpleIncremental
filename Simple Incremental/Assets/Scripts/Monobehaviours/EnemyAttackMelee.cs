847f0f88fe4e9d26cca2041deed6a74fee56d7a3
Erik <>
2019-03-18 21:38:32 -0500

    If collided with player, deal melee damage and bop them

---
+++ Simple Incremental/Assets/Scripts/Monobehaviours/EnemyAttackMelee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackMelee : MonoBehaviour
+{
+    Rigidbody2D rb2d = null;
+    public int damage = 1;
+    public float punchForce = 200f;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CharacterHealth ch = collision.gameObject.GetComponent<CharacterHealth>();
+            Vector2 forcePush = (ch.transform.position - transform.position).normalized;
+            Rigidbody2D targetRb2d = ch.GetComponent<Rigidbody2D>();
+            targetRb2d?.AddForce(forcePush * punchForce);
+            rb2d?.AddForce(-forcePush * punchForce);
+            ch.TakeDamage(damage);
+        }
+    }
+}
