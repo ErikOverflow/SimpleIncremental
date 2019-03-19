5dda5d13ae4a1887bcefb1b050b75968dec20e62
Erik <>
2019-03-18 21:36:24 -0500

    Added ranged reload time and melee fields

--- Simple Incremental/Assets/Scripts/ScriptableObjects/EnemyTemplate.cs
+++ Simple Incremental/Assets/Scripts/ScriptableObjects/EnemyTemplate.cs
@@ -9,11 +9,17 @@ public class EnemyTemplate : ScriptableObject
     public Sprite sprite = null;
     public int health = 10;
     public int coins = 10;
+
     public float moveSpeed = 1f;
     public float responseTime = 1f;
+
     public Sprite projectileSprite = null;
     public int rangedDamage = 1;
     public float rangedFalloffTime = 3f;
+    public float reloadTime = 1f;
+
     public int maxPenetrations = 1;
     public float projectileSpeed = 3f;
+    public int meleeDamage = 1;
+    public float meleePunchForce = 200f;
 }
