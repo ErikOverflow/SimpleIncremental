a04027ecc9aceb2b644fea6c1100db3415841e0f
Erik <>
2019-04-07 21:19:21 -0500

    Modifications to allow the attack to last multiple frames based on animation.

--- Simple Incremental/Assets/Scripts/Monobehaviours/WeaponMeleeController.cs
+++ Simple Incremental/Assets/Scripts/Monobehaviours/WeaponMeleeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,18 +9,57 @@ public class WeaponMeleeController : MonoBehaviour
     [NonSerialized]
     public GameObject weapon;
     [SerializeField]
-    ContactFilter2D cf2d = new ContactFilter2D();
+    LayerMask mask;
+    ContactFilter2D cf2d;
+    Collider2D[] colliders;
+    Animator anim;
+    bool canAttack;
+    List<CharacterHealth> damagedCharacters = new List<CharacterHealth>();
+
+    private void OnDisable()
+    {
+        canAttack = true;
+    }
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        cf2d = new ContactFilter2D();
+        cf2d.layerMask = mask;
+        cf2d.useLayerMask = true;
+        colliders = new Collider2D[10];
+        canAttack = true;
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && Time.timeScale != 0)
         {
-            Collider2D[] colliders = new Collider2D[5];
-            weapon.GetComponent<Collider2D>().OverlapCollider(cf2d, colliders);
-            foreach (Collider2D col in colliders)
+            StartCoroutine(CheckForAttacking());
+        }
+    }
+
+    private IEnumerator CheckForAttacking()
+    {
+        if (canAttack)
+        {
+            canAttack = false;
+            damagedCharacters.Clear();
+            while (anim.GetBool("Attacking"))
             {
-                col?.GetComponent<CharacterHealth>()?.TakeDamage(damage);
+                weapon.GetComponent<Collider2D>().OverlapCollider(cf2d, colliders);
+                foreach (Collider2D col in colliders)
+                {
+                    CharacterHealth ch = col?.GetComponent<CharacterHealth>();
+                    if (!damagedCharacters.Contains(ch))
+                    {
+                        ch?.TakeDamage(damage);
+                        damagedCharacters.Add(ch);
+                    }
+                }
+                yield return new WaitForFixedUpdate();
             }
+            canAttack = true;
         }
     }
 }
