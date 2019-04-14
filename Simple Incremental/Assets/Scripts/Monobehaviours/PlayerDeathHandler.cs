daec61634295c9aa9426fb985ff02ffe2c4c0ca1
Raganvald <raganvald@gmail.com>
2019-04-13 23:51:15 -0700

    Fix the girls death animation to be triggered.

---
+++ Simple Incremental/Assets/Scripts/Monobehaviours/PlayerDeathHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathHandler : MonoBehaviour
+{
+    CharacterHealth ch = null;
+    Animator anim;
+    [SerializeField]
+    int deathHash = Animator.StringToHash("Death");
+
+    private void Awake()
+    {
+        ch = GetComponent<CharacterHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        ch.OnDeath += PlayerDied;
+    }
+
+    public void PlayerDied()
+    {
+        anim.SetTrigger(deathHash);
+    }
+}
