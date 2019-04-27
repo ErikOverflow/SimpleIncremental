6d30f692450472684fcbc057dd8ddfed8dbda01d
ErikOverflow <ErikOverflow@gmail.com>
2019-04-26 22:10:58 -0500

    Add smoother animations

--- Simple Incremental/Assets/Scripts/Monobehaviours/PlayerMovementController.cs
+++ Simple Incremental/Assets/Scripts/Monobehaviours/PlayerMovementController.cs
@@ -2,58 +2,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Animator))]
+
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] string horizontalAxis = "Horizontal";
-    [SerializeField] float horizontalSpeed = 3f;
+    [SerializeField] float maxSpeed = 3f;
+    [SerializeField] float acceleration = 100f;
     [SerializeField] private float jumpForce = 400f;
-    [Range(0, .5f)] [SerializeField] private float horizontalSmoothing = .05f;
 
     public LayerMask groundLayer;
 
     float horizontalForce;
     Rigidbody2D rigidBody;
     Vector2 currentVelocity = Vector2.zero;
-    Animator anim;
-    int groundedHash = Animator.StringToHash("Grounded");
     CharacterHealth ch = null;
+    Animator anim = null;
+    bool grounded = true;
 
     private void Awake()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
         ch = GetComponent<CharacterHealth>();
-    }
-
-    private void Start()
-    {
-        ch.OnDeath += DisableInput;
     }
 
     private void Update()
     {
-        horizontalForce = Input.GetAxisRaw(horizontalAxis);
+        horizontalForce = Input.GetAxis(horizontalAxis);
 
-        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool(groundedHash))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            anim.SetTrigger("Jump");
-            rigidBody.AddForce(new Vector2(0f, jumpForce));
+            Jump();
+        }
+        anim.SetBool("Running", horizontalForce != 0);
+        if(horizontalForce != 0)
+        {
+            anim.SetBool("FacingRight", horizontalForce > 0);
         }
     }
+
+    private void Jump()
+    {
+        rigidBody.AddForce(new Vector2(0f, jumpForce));
+        grounded = false;
+    }
+
     private void FixedUpdate()
     {
-        var targetVelocity = new Vector2(horizontalForce * horizontalSpeed, rigidBody.velocity.y);
-        rigidBody.velocity = Vector2.SmoothDamp(rigidBody.velocity, targetVelocity, ref currentVelocity, horizontalSmoothing);
-
-        anim.SetFloat("VelocityX", Math.Abs(targetVelocity.x));
-
-        // Flip sprite based on movement direction
-        if(horizontalForce < 0 && transform.localScale.x < 0 || horizontalForce > 0 && transform.localScale.x > 0)
+        if (horizontalForce != 0)
         {
-            transform.localScale = new Vector3(-1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            rigidBody.AddForce(horizontalForce * Vector2.right * acceleration);
+        }
+        Vector2 vel = rigidBody.velocity;
+        if(Mathf.Abs(vel.x) > maxSpeed)
+        {
+            rigidBody.velocity = new Vector2(maxSpeed * Mathf.Sign(vel.x), vel.y);
         }
     }
 
@@ -61,20 +65,7 @@ public class PlayerMovementController : MonoBehaviour
     {
         if (groundLayer == (groundLayer | ( 1<< col.gameObject.layer)))
         {
-            anim.SetBool(groundedHash, true);
+            grounded = true;
         }
-    }
-
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (groundLayer == (groundLayer | (1 << col.gameObject.layer)))
-        {
-            anim.SetBool(groundedHash, false);
-        }
-    }
-
-    public void DisableInput()
-    {
-        enabled = false;
     }
 }
